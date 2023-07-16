44c5655f4ac0f4a2df68a1a50152b2a4d91e2011
EvansOdhams <evansodhams@gmail.com>
2023-07-16 11:20:05 +0300

    Add 0x01-string_manipulation.py

---
+++ day-1/0x01-string_manipulation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+Here goes String Indexing
+"""
+
+message = "Hello, World!"
+print(message[0])#Output: "H" (First Character"
+print(message[7])#Output: "O" (8th Character)
