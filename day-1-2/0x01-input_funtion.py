b1eaf4cba208ea62fe01d925ba7d771238b1ee82
EvansOdhams <evansodhams@gmail.com>
2023-07-18 12:39:19 +0300

    Add Python Input Functions

---
+++ day-1-2/0x01-input_funtion.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Input functions
+"""
+
+name = input("Enter your Name: ")
+
+print("Hello " + name)
---
+++ day-1-2/input_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""
+Python input functions
+"""
+
+print("Hello " + input("What is your Name?"))
