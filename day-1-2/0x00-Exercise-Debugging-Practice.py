8e4122ec321419fcf1b3b1d4b4657c8d160e0af8
EvansOdhams <evansodhams@gmail.com>
2023-07-18 12:20:55 +0300

    Add 0x00-Exercise-Debugging-Practice.py

---
+++ day-1-2/0x00-Exercise-Debugging-Practice.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Python Debugging Exercises
+"""
+
+#1. Missing double quotes before the word day
+print(Day 1 - String Manipulation")
+
+#2. Outer double quotes changed to single quotes
+print('string concatenantion is done with the "+" sign.')
+
+#3. Extra indentation removed
+print('e.g. print("Hello " + "World")')
+
+#4. Extra print function removed.
+print("New lines can be created with a bcklasha and n.")
+
