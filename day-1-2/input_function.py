4c31fd6108cdc43335c1babb6208d412b58b209f
EvansOdhams <evansodhams@gmail.com>
2023-07-18 12:44:46 +0300

    Add input_function.py

--- day-1-2/input_function.py
+++ day-1-2/input_function.py
@@ -4,4 +4,6 @@
 Python input functions
 """
 
+# Input() will get the user's input in the console
+# The print() will print the word "Hello" and the user's input
 print("Hello " + input("What is your Name?"))
